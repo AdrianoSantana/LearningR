f9a39683b5914b4c1c8bad36a344034397baae8c
ans3 <ans3@cin.ufpe.br>
2020-07-23 07:36:45 -0300

    Creating arquivos submit semana 2

---
+++ makeCacheMatrix.R
@@ -0,0 +1,26 @@
+makeCacheMatrix <- function(x = numeric()) {
+  inverseValueMatrix <- NULL
+  set <- function(y) {
+    x <<- y
+    inverseValueMatrix <<- NULL
+  }
+  
+  get <- function() x
+  
+  setInverse <- function(inverseValue) inverseValueMatrix <<- inverseValue
+
+  
+  getInverse <- function() inverseValueMatrix
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+}
+
+cacheSolve <- function(x) {
+  inverseValueMatrix <- x$getInverse()
+  if (!is.null(inverseValueMatrix)) {
+    return (inverseValueMatrix)
+  }
+  myMatrix <- x$get()
+  aux <- solve(myMatrix)
+  x$setInverse(aux)
+  aux
+}
